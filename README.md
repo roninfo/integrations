c1e8948a8f5c81841cfd80c2a825f3b16b910cbe
Roninfo <roninfo@gmail.com>
2019-07-13 16:35:17 -0300

    remove all files

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 study grouping about integrations
 Branch base for grouping
 
+
+
+
--- src/main/java/br/com/roninfo/graphqldemo/repository/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.roninfo.graphqldemo.repository;
-
-import br.com.roninfo.graphqldemo.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book,String> {
-
-}
